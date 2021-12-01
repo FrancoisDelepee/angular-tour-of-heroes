93f04ebc2b7219f882499ccc390b1d3875c4d499
Francois.Delepee <francoisdelepee@gmail.com>
2021-12-01 11:14:10 +0100

    tour of heroes part 2 done

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../heros";
+import {HEROES} from "../mock-heroes";
 
 
 @Component({
@@ -9,14 +10,16 @@ import {Hero} from "../heros";
 })
 export class HeroesComponent implements OnInit {
 
-  hero : Hero = {
-    id : 1,
-    name : 'Windstorm'
-  };
+  heroes = HEROES;
+
+  selectedHero? : Hero;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSelect(hero : Hero) : void{
+    this.selectedHero = hero;
+  }
 }
